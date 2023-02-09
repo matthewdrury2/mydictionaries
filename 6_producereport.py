d483ea02312dd8bca8a213d423943bcadde59901
Matthew Drury <matthew_drury2@baylor.edu>
2023-02-08 23:32:09 -0600

    Update

--- 6_producereport.py
+++ 6_producereport.py
@@ -4,7 +4,8 @@ since it was manually entered. Print out the details of any produce that has in 
 as well as the calculated total that shows the discrepancy.
 '''
 
-ProduceDictionary={
+
+ProduceDictionary = {
     'Potatoes': {
         'cost': 0.86,
         'amt_sold': 39.8,
@@ -48,7 +49,7 @@ ProduceDictionary={
     'Celery': {
         'cost': 3.07,
         'amt_sold': 18.5,
-        'total': 56.8
+        'total': 56.79
     },
     'Spinach': {
         'cost': 4.12,
@@ -193,7 +194,7 @@ ProduceDictionary={
     'Brussels sprouts': {
         'cost': 1.65,
         'amt_sold': 22.9,
-        'total': 37.79
+        'total': 37.78
     },
     'Kale': {
         'cost': 5.02,
@@ -207,3 +208,14 @@ ProduceDictionary={
     }
 }
 
+for produce in ProduceDictionary:
+    cost = ProduceDictionary[produce]['cost']
+    amt_sold = ProduceDictionary[produce]['amt_sold']
+    stated_total = ProduceDictionary[produce]['total']
+    calc_total = round(cost * amt_sold, 2)
+    if calc_total != stated_total:
+        print(f'Produce Name: {produce}')
+        print(f'Calculated Total: ${calc_total: ,.2f}')
+        print(f'Calculated Total: ${stated_total: ,.2f}')
+        print()
+        print()
